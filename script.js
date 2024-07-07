9576f3be6b5c9cfdf40a40adc359b43e6a7fa24f
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-07-07 13:11:56 +0500

    Add files via upload
    🤠

---
+++ script.js
@@ -0,0 +1,22 @@
+var schools=["Islamia School","Jinnah school","Habib School"];
+var school=prompt("Enter any School")
+
+if(schools[0]===school){alert(schools[0])}
+else if(schools[1]===school){alert(schools[1])}
+else if(schools[2]===school){alert(schools[2])}
+
+else{
+    alert("This is not a correction")
+}
+
+
+
+
+
+
+
+
+
+
+
+
